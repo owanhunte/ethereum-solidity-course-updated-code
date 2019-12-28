8adcb631177ea217c2c3526ee6a9f39186a90771
Owan Hunte <owanh@hotmail.com>
2019-12-28 19:25:38 -0400

    Adds the ability for players to enter the lottery.

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -2,8 +2,18 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     constructor() public {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(
+            msg.value > .01 ether,
+            "A minimum payment of .01 ether must be sent to enter the lottery"
+        );
+
+        players.push(msg.sender);
     }
 }
