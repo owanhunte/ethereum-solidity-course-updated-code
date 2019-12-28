17a060f488aa529e60f3a50ee9fd369685c72af9
Owan Hunte <owanh@hotmail.com>
2019-12-28 02:40:15 -0400

    Adds comment on interfacing with the Solidity compiler.

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,6 +5,13 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
+/***
+ * The recommended way to interface with the Solidity compiler, especially for more
+ * complex and automated setups is the so-called JSON-input-output interface.
+ *
+ * See https://solidity.readthedocs.io/en/v0.6.0/using-the-compiler.html#compiler-input-and-output-json-description
+ * for more details.
+ */
 const input = {
   language: "Solidity",
   sources: {
