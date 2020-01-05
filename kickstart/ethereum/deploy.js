327990bcf226c629f45d31726ec2882375ff6c06
Owan Hunte <owanh@hotmail.com>
2020-01-05 04:05:17 -0400

    Adds deploy script for the kickstart project.

---
+++ kickstart/ethereum/deploy.js
@@ -0,0 +1,25 @@
+// Load environment variables.
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+const mnemonic = process.env.ACCOUNT_MNEMONIC;
+const network = process.env.RINKEBY_ENDPOINT;
+
+const provider = new HDWalletProvider(mnemonic, network);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: "0x" + compiledFactory.evm.bytecode.object })
+    .send({ from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
