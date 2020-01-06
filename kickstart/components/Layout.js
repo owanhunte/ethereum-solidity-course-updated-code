3c7d11e912d0fb5479be351dc95de53a220f1091
Owan Hunte <owanh@hotmail.com>
2020-01-06 02:13:26 -0400

    CSS Fix for the CrowdCoin app theme.

--- kickstart/components/Layout.js
+++ kickstart/components/Layout.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import Head from "next/head";
 import Header from "./Header";
 
 export default props => {
   return (
     <Container>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+      </Head>
+
       <Header />
       {props.children}
     </Container>
--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -28,10 +28,6 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link
-            rel="stylesheet"
-            href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-          />
           <h3>Open Campaigns</h3>
           <Button
             floated="right"
