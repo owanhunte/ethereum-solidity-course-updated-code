1c42244141125ec34955d80d27f8c7a5c0217d70
Owan Hunte <owanh@hotmail.com>
2021-06-09 01:23:01 -0400

    refactor: update Inbox.sol smart contract
    Key update points:
    * Update `pragma` line.
    * Visibility ( public / external ) not needed for constructors anymore.
    * Add comments that mention state variable `public` keyword side effect.

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,13 +1,29 @@
-pragma solidity >=0.5.0 <0.8.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
 
 contract Inbox {
+    // The keyword public automatically generates a function that
+    // allows you to access the current value of the state variable
+    // from outside of the contract (see lines 22 - 28).
     string public message;
 
-    constructor(string memory initialMessage) public {
+    // Note: Removing `public` visibility specifier. Visibility (public / external)
+    // is not needed for constructors anymore: To prevent a contract from being
+    // created, it can be marked abstract. This makes the visibility concept
+    // for constructors obsolete.
+    constructor(string memory initialMessage) {
         message = initialMessage;
     }
 
     function setMessage(string memory newMessage) public {
         message = newMessage;
     }
+
+    // Because we declared the `message` state variable above with
+    // keyword `public`, the compiler automatically generates a getter
+    // function for us equivalent to:
+    //
+    // function message() external view returns (string) { return message; }
+    //
+    // ...so we do **NOT** need to define a getter function ourselves.
 }
