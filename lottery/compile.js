09587e7d4c3fb23835e847bd48c3ed62e8612cd9
Owan Hunte <owanh@hotmail.com>
2021-06-17 14:03:11 -0400

    refactor: update Lottery project's compile.js

--- lottery/compile.js
+++ lottery/compile.js
@@ -9,17 +9,23 @@ const source = fs.readFileSync(lotteryPath, "utf8");
  * The recommended way to interface with the Solidity compiler, especially for more
  * complex and automated setups is the so-called JSON-input-output interface.
  *
- * See https://solidity.readthedocs.io/en/v0.5.15/using-the-compiler.html#compiler-input-and-output-json-description
+ * See https://docs.soliditylang.org/en/v0.8.5/using-the-compiler.html#compiler-input-and-output-json-description
  * for more details.
  */
 const input = {
   language: "Solidity",
   sources: {
+    // Each Solidity source file to be compiled must be specified by defining either
+    // a URL to the file or the literal file content.
+    // See https://docs.soliditylang.org/en/v0.8.5/using-the-compiler.html#input-description
     "Lottery.sol": {
       content: source
     }
   },
   settings: {
+    metadata: {
+      useLiteralContent: true
+    },
     outputSelection: {
       "*": {
         "*": ["*"]
