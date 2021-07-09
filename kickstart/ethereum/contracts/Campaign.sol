b006f1bc98ca1a62ffaceb84086cb2040073e97e
Owan Hunte <owanh@hotmail.com>
2021-07-09 18:50:37 -0400

    refactor(CrowdCoin): update Campaign smart contract for Solidity v0.8 compatibility

--- kickstart/ethereum/contracts/Campaign.sol
+++ kickstart/ethereum/contracts/Campaign.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.0 <0.7.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
 
 contract CampaignFactory {
     Campaign[] public deployedCampaigns;
@@ -27,9 +28,29 @@ contract Campaign {
     uint256 public minimumContribution;
     mapping(address => bool) public approvers;
     uint256 public approversCount;
-    Request[] public requests;
 
-    constructor(uint256 minimum, address creator) public {
+    // As of Solidity 0.7.0, an unsafe feature related to mappings in Structs was removed.
+    // If a struct or array contains a mapping, it can only be used in storage. Prior to 0.7.0,
+    // mapping members were silently skipped in memory, which is confusing and error-prone.
+    //
+    // So the line which is given in the course code:
+    //
+    // Request[] public requests;
+    //
+    // now becomes the following 2 lines of code. Also in the createRequest
+    // function, instead of creating a memory Struct, which the code was proviously
+    // doing, we now have to create a storage Struct.
+    //
+    // And finally, wherever the course code has the line `requests.length` gets replaced
+    // by `numRequests`.
+    //
+    // https://docs.soliditylang.org/en/v0.7.0/070-breaking-changes.html#mappings-outside-storage
+    // https://docs.soliditylang.org/en/v0.8.6/types.html?highlight=struct#structs
+    //
+    uint256 numRequests;
+    mapping(uint256 => Request) requests;
+
+    constructor(uint256 minimum, address creator) {
         manager = creator;
         minimumContribution = minimum;
     }
@@ -48,15 +69,12 @@ contract Campaign {
         uint256 value,
         address payable recipient
     ) public onlyManager {
-        Request memory newRequest =
-            Request({
-                description: description,
-                value: value,
-                recipient: recipient,
-                complete: false,
-                approvalCount: 0
-            });
-        requests.push(newRequest);
+        Request storage r = requests[numRequests++];
+        r.description = description;
+        r.value = value;
+        r.recipient = recipient;
+        r.complete = false;
+        r.approvalCount = 0;
     }
 
     function approveRequest(uint256 index) public {
@@ -100,14 +118,14 @@ contract Campaign {
         return (
             minimumContribution,
             address(this).balance,
-            requests.length,
+            numRequests,
             approversCount,
             manager
         );
     }
 
     function getRequestsCount() public view returns (uint256) {
-        return requests.length;
+        return numRequests;
     }
 
     modifier onlyManager() {
